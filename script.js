523e50044385fd65d4c81368c6b90ba46400b4dc
Fae321 <137207937+Fae321@users.noreply.github.com>
2023-07-10 20:27:23 -0400

    Project Page
    Added replit project page to vs code

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 let menu = document.querySelector(".menu-img");
 let navBar = document.querySelector(".navbar");
 let exitMenu = document.querySelector(".closebtn");
+let projectButton = document.querySelector(".project-but");
+let homeButton = document.querySelector(".home-but");
+let aboutMeButton = document.querySelector(".aboutme-but");
+let contactButton = document.querySelector(".contact-but");
+let homePage = document.querySelector(".home");
+let aboutMePage = document.querySelector(".aboutme");
+let projectPage = document.querySelector(".projects");
+let contactPage = document.querySelector(".contacts");
+let likeButtons = document.querySelectorAll("#like-but");
+
 
 menu.onclick= function() {
     navBar.style.width = "250px";
@@ -8,4 +18,39 @@ menu.onclick= function() {
 
 exitMenu.onclick = function () {
     navBar.style.width = "0";
+}
+
+for (let button of likeButtons) {
+    button.onclick = function() {
+      button.classList.toggle("glyphicon-heart-empty");
+      button.classList.toggle("glyphicon-heart");
+    };
+  };
+
+homeButton.onclick = function () {
+    homePage.style.display = "block";
+    aboutMePage.style.display = "none";
+    projectPage.style.display = "none";
+    contactPage.style.display = "none";
+   
+}
+aboutMeButton.onclick = function () {
+    homePage.style.display = "none";
+    aboutMePage.style.display = "block";
+    projectPage.style.display = "none";
+    contactPage.style.display = "none";
+    
+}
+projectButton.onclick = function () {
+    homePage.style.display = "none";
+    aboutMePage.style.display = "none";
+    projectPage.style.display = "block";
+    contactPage.style.display = "none";
+   
+}
+contactButton.onclick = function () {
+    homePage.style.display = "none";
+    aboutMePage.style.display = "none";
+    projectPage.style.display = "none";
+    contactPage.style.display = "block";
 }
